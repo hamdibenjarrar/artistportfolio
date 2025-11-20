62b58920edc573d95259ff6ccfe0aba938ab378e
Hamdi Ben Jarrar <hamdibenjarrar@gmail.com>
2025-11-20 20:53:25 +0100

    Clean up unused files and components

--- content/initialArtworks.ts
+++
@@ -1,7 +0,0 @@
-export const INITIAL_ART_IMAGES: string[] = [
-  "/work/yas2.jpg", // second portrait treated as artwork
-  "/work/p1/p1.jpg",
-  "/work/p2/p2.jpg",
-  "/work/p3/p3.jpg",
-  "/work/p4/IMG_8369_result.jpg"
-];
