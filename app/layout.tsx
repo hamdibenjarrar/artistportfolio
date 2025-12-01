8dfb9e55b1cdb652ec72826a02be98fed4947b4a
Hamdi Ben Jarrar <hamdibenjarrar@gmail.com>
2025-12-01 17:02:43 +0100

    Use favicon.ico for all icon types

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,11 @@ export const metadata: Metadata = {
       },
     ],
   },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
